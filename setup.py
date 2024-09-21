dff36b16459c939f82033013c5b00cc0e4a23287
botcaps <Abhishek.7979883@gmail.com>
2024-09-21 19:57:10 +0530

    Second Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup (
+    name = 'New Project',
+    version='0.0.1',
+    author='Abhishek',
+    author_email='abhishek.7979883@gmail.com',
+    packages=find_packages(),
+    install_requires = ['Numpy','Pandas']
+)
