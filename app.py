6eaa45af0dc89d9ed5307fdeedb3cdd81b4a943f
botcaps <Abhishek.7979883@gmail.com>
2024-09-29 00:09:14 +0530

    new commit data ingestion

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 import sys
 import os
-
 # Add the src directory to the Python path
 sys.path.append(os.path.join(os.getcwd(), 'src'))
 
 from Newproject.logger import logging
 from Newproject.exception import customException
+from Newproject.components.data_ingestion import DataIngestion
+from Newproject.components.data_ingestion import DataIngestionConfig
+
+
 
 if __name__ == "__main__":
     logging.info('Logging info has started')
 
     try:
-        a = 1 / 0  # This will raise a ZeroDivisionError
+        #data_ingestion = DataIngestionConfig()
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+
     except Exception as e:
         logging.error(f"An error occurred: {e}")  # Use logging.error to log the exception message
         raise customException(f"An error occurred: {e}", sys)  # Raise the CustomException with a message
